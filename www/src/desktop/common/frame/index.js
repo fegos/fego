ac3c09e46ef3e740cda3b198d217c46c94cf420a
asy <lucky_asyxu@163.com>
2018-03-08 15:13:39 +0800

    MOD:优化frame以及右侧菜单

--- www/src/desktop/common/frame/index.js
+++ www/src/desktop/common/frame/index.js
@@ -5,28 +5,85 @@ import Header from './Header';
 import Footer from './Footer';
 import './style/index.less';
 
-export default props => (
-  <div className="app">
-    <Header />
-    <div className="main">
-      <ul>
-        <li><Link to="/ui/Button">按钮 Button</Link></li>
-        <li><Link to="/ui/Card">卡片 Card</Link></li>
-        <li><Link to="/ui/Carousel">走马灯 Carousel</Link></li>
-        <li><Link to="/ui/Checkbox">复选框 Checkbox</Link></li>
-        <li><Link to="/ui/Dialog">对话框 Dialog</Link></li>
-        <li><Link to="/ui/Form">表单 Form</Link></li>
-        <li><Link to="/ui/Input">输入框 Input</Link></li>
-        <li><Link to="/ui/Loading">加载中 Loading</Link></li>
-        <li><Link to="/ui/Pagination">分页器 Pagination</Link></li>
-        <li><Link to="/ui/Radio">单选框 Radio</Link></li>
-        <li><Link to="/ui/Select">下拉框 Select</Link></li>
-        <li><Link to="/ui/Table">表格 Table</Link></li>
-      </ul>
-      <div className="page">
-        {props.children}
+const menus = [{
+  link: '/ui/Button',
+  title: 'Button',
+  subTitle: '按钮',
+}, {
+  link: '/ui/Card',
+  title: 'Card',
+  subTitle: '卡片',
+}, {
+  link: '/ui/Carousel',
+  title: 'Carousel',
+  subTitle: '走马灯',
+}, {
+  link: '/ui/Checkbox',
+  title: 'Checkbox',
+  subTitle: '复选框',
+}, {
+  link: '/ui/Dialog',
+  title: 'Dialog',
+  subTitle: '对话框',
+}, {
+  link: '/ui/Form',
+  title: 'Form',
+  subTitle: '表单',
+}, {
+  link: '/ui/Input',
+  title: 'Input',
+  subTitle: '输入框',
+}, {
+  link: '/ui/Loading',
+  title: 'Loading',
+  subTitle: '加载中',
+}, {
+  link: '/ui/Pagination',
+  title: 'Pagination',
+  subTitle: '分页器',
+}, {
+  link: '/ui/Radio',
+  title: 'Radio',
+  subTitle: '单选框',
+}, {
+  link: '/ui/Select',
+  title: '下拉框',
+  subTitle: 'Select',
+}, {
+  link: '/ui/Table',
+  title: '表格',
+  subTitle: 'Table',
+}];
+
+function sort(menu1, menu2) {
+  if (menu1.title < menu2.title) {
+    return -1;
+  } else if (menu1.title > menu2.title) {
+    return 1;
+  }
+  return 0;
+}
+
+export default (props) => {
+  const { pathname } = window.location;
+  return (
+    <div className="app">
+      <Header />
+      <div className="main">
+        <ul>
+          {
+            menus.sort(sort).map(menu => (
+              <li key={menu.link} className={pathname.indexOf(menu.link) >= 0 ? 'active' : ''}>
+                <Link to={menu.link}>{menu.subTitle} {menu.title}</Link>
+              </li>
+            ))
+          }
+        </ul>
+        <div className="page">
+          {props.children}
+        </div>
       </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-);
+  );
+};
