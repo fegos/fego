d510b0bd5d75d3efcf65a9ab4fb737cdf0f96b2c
asy <lucky_asyxu@163.com>
2018-03-15 17:06:28 +0800

    ADD:引入enzyme

--- __tests__/Button/index.test.js
+++ __tests__/Button/index.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Button from 'Button';
 
 describe('button test', () => {
   it('button snapshot', () => {
-    const wrapper = Renderer.create((<Button />)).toJSON();
+    const wrapper = Renderer.create((<Button>button</Button>)).toJSON();
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('fires pressIn event', () => {
+    const onClickFunc = jest.fn();
+    const button = shallow(<Button onClick={onClickFunc}>button</Button>);
+    button.simulate('click');
+
+    expect(onClickFunc).toBeCalled();
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,14 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'], // 查找包的路径
   moduleFileExtensions: ['js', 'jsx'], // 文件扩展名
   notify: true,
+  /**
+   * setupFiles vs setupTestFrameworkScriptFile
+   * https://facebook.github.io/jest/docs/en/configuration.html#setupfiles-array
+   * setupFiles 里的代码执行时机在 test environment installed 之前
+   * setupTestFrameworkScriptFile 里的代码执行时机在 test environment installed 之后
+   */
+  // setupFiles: ['./enzyme.setup.js'], // enzyme 启动配置文件
+  setupTestFrameworkScriptFile: './enzyme.setup.js', // enzyme 启动配置文件
   testPathIgnorePatterns: ['node_modules', 'site', 'www'], // 搜索测试用例时忽略的路径
   testRegex: '(/__tests__/.*test)\\.jsx?$', // 匹配测试文件，当前指定为以 .test.js(x) 结尾的文件
   verbose: true, // 在执行测试用例期间就报告每个测试用例的执行结果
